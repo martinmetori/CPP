4db62636bdf4edd26d4a717b14eabb869940d7d4
Martin Metori <39520248+martinmetori@users.noreply.github.com>
2018-05-24 13:01:11 +0300

    Create colors.cpp

---
+++ Colors/colors.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()	{
+	system ("pause");
+	cout <<"\n\n\t\t";
+	
+	system("color 01");
+	Sleep(2000);
+cout <<"Lorem ipsum dolor sit amet,";
+	system("color 02");
+	Sleep(2000);
+cout <<"consectetur adipiscing elit,";
+	system("color 03");
+	Sleep(2000);
+cout <<"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+	system("color 04");
+	Sleep(2000);
+cout <<"Ut enim ad minim veniam,";
+	system("color 05");
+	Sleep(2000);
+cout <<"quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+	system("color 06");
+	Sleep(2000);
+cout <<"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+	system("color 07");
+	Sleep(2000);
+cout <<"Excepteur sint occaecat cupidatat non proident,";
+	system("color 08");
+	Sleep(2000);
+cout <<"sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	system("color 09");
+	Sleep(2000);
+
+		system("color 0A");
+		Sleep(2000);
+cout <<"Maecenas sed enim ut sem viverra.";
+		system("color 0B");
+		Sleep(2000);
+cout <<"Neque ornare aenean euismod elementum nisi.";
+		system("color 0C");
+		Sleep(2000);
+cout <<"Eget nunc scelerisque viverra mauris.";
+		system("color 0D");
+		Sleep(2000);
+cout <<"Elit ullamcorper dignissim cras tincidunt. ";
+		system("color 0E");
+		Sleep(2000);
+cout <<"Sit amet nisl suscipit adipiscing bibendum est ultricies integer. ";
+		system("color 0F");
+		Sleep(2000);
+
+	
+	cout << "\n\n\t";
+	system("pause");
+}
